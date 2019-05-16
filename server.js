32dd1be18d268010267b27ad5f87f5f2087e11eb
Sujit Sharma <sujit.dhakal1166@gmail.com>
2019-05-16 08:34:25 +0545

    changed

--- server.js
+++ server.js
@@ -1,34 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const express = require('express');
 
-
+const Routes = require('./routes/bookRoute');
+const db = require('./dbconnection');
 
 let Book = require('./model/book');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.get('/',(req,res) => {
-    Book.find(function(err, bookList) {
-        if(err) res.send({error: err.message})
-        console.log(bookList, '>>>>')
-        res.send(bookList)
-    })
-});
 
-app.post('/',(req,res) => {
-    console.log(req.body);
-    const data = req.body;
-    Book.create(data, (err,createData)=> {
-        if(err) res.send({error: err.message})
-        res.send(createData)
-    })
-   
-});
+app.use(Routes);
 
-mongoose.connect('mongodb://localhost/sujit');
-let db = mongoose.connection;
 
 // Check for Db errors
 db.on('error', (err) => console.log(err))
@@ -37,4 +20,4 @@ db.on('error', (err) => console.log(err))
 db.once('open', () =>  {
     console.log("connected to mongodb")
     app.listen(3001,() => console.log("server running at 3001"));
-})
+});
